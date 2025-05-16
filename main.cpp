97543319ccc41fd1016eb08941f2baa16c8f3a6f
ali.ammar2 <ali.ammar@baly.iq>
2025-05-16 14:15:08 +0300

    ref: include welcome inside the code instead of seperate txt file

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 string usage(){
     return "Usage: ./main \n\texample:\n\tEnter your lineup(x-y-z): 4-3-3";
 }
-void print_row(int count, int total_width = 50, string icon = "●") {
+void print_row(int count, int total_width = 50, string icon = "*") {
     int spacing = (total_width - (count * 4)) / 2;  
     cout << setw(spacing) << "";  
 
@@ -16,30 +16,37 @@ void print_row(int count, int total_width = 50, string icon = "●") {
     }
     cout << endl;
 }
-void welcome(const string& filename){
-    ifstream file(filename);
-    if (!file.is_open()) {
-        cerr << "Error: Could not open file: " << filename << endl;
-        return;
-    }
-
-    string line;
-    while (getline(file, line)) {
-        cout << line << endl;
-    }
-    file.close();
+void welcome(){
+    cout << "   ___               __    __                ___    ___      " << endl;
+    cout << " /'___\\             /\\ \\__/\\ \\              /\\_ \\  /\\_ \\     " << endl;
+    cout << "/\\ \\__/  ___     ___\\ \\ ,_\\ \\ \\____     __  \\\\ /\\ \\ \\\\/\\ \\    " << endl;
+    cout << "\\ \\ ,__/\\/ __`\\  / __`\\ \\ \\/\\ \\ '__`\\  /'__`\\  \\ \\ \\  \\ \\ \\   " << endl;
+    cout << " \\ \\ \\_/\\ \\L\\ \\/\\ \\L\\ \\ \\ \\_\\ \\ \\L\\ \\/\\ \\L\\.\\_ \\_\\ \\_ \\_\\ \\_ " << endl;
+    cout << "  \\ \\_\\\\ \\____/\\ \\____/\\ \\__\\\\ \\_,__/\\ \\__/.\\_\\/\\____\\/\\____\\ " << endl;
+    cout << " __\\/_/ \\/___/  \\/___/  \\/__/ \\/___/  \\/__/\\/_/\\/____/\\/____/ " << endl;
+    cout << "/\\_ \\    __                                                  " << endl;
+    cout << "\\/\\ \\  /\\_\\    ___      __   __  __  _____                  " << endl;
+    cout << "  \\ \\ \\ \\/\\ \\ /' _ `\\  /'__`\\/\\ \\/\\ \\/\\ '__`\\                " << endl;
+    cout << "   \\_\\ \\_\\ \\ \\/\\ \\/\\ \\/\\  __/\\ \\ \\_\\ \\ \\ \\L\\ \\               " << endl;
+    cout << "   /\\____\\\\ \\_\\ \\_\\ \\_\\ \\____\\\\ \\____/\\ \\ ,__/               " << endl;
+    cout << "   \\/____/ \\/_/\\/_/\\/_/\\/____/ \\/___/  \\ \\ \\/                " << endl;
+    cout << "                                        \\ \\_\\                " << endl;
+    cout << "                                         \\/_/                " << endl;
+    cout << "======================================================================" << endl;
+    cout << "  CLI to visualise football lineup when user inputs a lineup format   " << endl;
+    cout << "======================================================================" << endl;
+    cout << "    By: Group A (Ali Ammar Kareem, Aymen Ahmed, Amjed Mahmoud)       " << endl;
+    cout << "======================================================================" << endl;
 }
 
 int main(){
-    system("clear");
     int dash = 0;
     int total = 0;
     int lineupArr[3];
     string lineup = ""; 
-    welcome("ascii.txt");
+    welcome();
     cout << "Enter your lineup(x-y-z): ";
     cin >> lineup;
-    system("clear");
     if(lineup.length() != 5){
         cout << "[ERROR] Invalid lineup" << endl;
         cout << usage() << endl;
@@ -66,7 +73,7 @@ int main(){
         print_row(lineupArr[i]);
         cout << endl;
     }
-    print_row(1,50,"■");
+    print_row(1,50,"0");
     cout << endl;
     return 0;
 }
